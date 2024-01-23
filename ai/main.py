0fab34f8df3e84c50568634cd5ce8a55ca615286
abbmarand <markus.andersson@abbgymnasiet.se>
2024-01-23 13:55:41 +0100

    gpt

--- ai/main.py
+++ ai/main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
-
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline, GPT2Tokenizer, TFGPT2LMHeadModel
 app = Flask(__name__)
-model_name = "deepset/roberta-base-squad2"
-nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+roberta = "deepset/xlm-roberta-large-squad2"
+qapipe = pipeline('question-answering', model=roberta, tokenizer=roberta)
+tokenizer = AutoTokenizer.from_pretrained("Intel/dynamic_tinybert")
+gpttokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = AutoModelForQuestionAnswering.from_pretrained("Intel/dynamic_tinybert")
+gptmodel = TFGPT2LMHeadModel.from_pretrained('gpt2')
 @app.route('/qa', methods=['POST'])
 def answer_question():
     data = request.get_json()
@@ -14,9 +15,17 @@ def answer_question():
     QA_input = {
     'question': question,
     'context': context,}
-    result = nlp(QA_input)
+    result = qapipe(QA_input)
     return jsonify(result)
+
+@app.route('/gpt', methods=['POST'])
+def gpt_ans():
+    data = request.get_json()
+    question = data.get('q')
+    encoded_input = gpttokenizer(question, return_tensors='tf')
+    output = gptmodel.generate(**encoded_input)
+    decoded = gpttokenizer.decode(output[0])
+    return decoded
 
 if __name__ == '__main__':
     app.run(debug=True)
-
