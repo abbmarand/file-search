8dd4d5a17d95b76845c5b6c5f23b5076e531545c
abbmarand <markus.andersson@abbgymnasiet.se>
2024-03-13 21:15:00 +0100

    chadcn and file data in UI

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
     import axios from "axios";
     import Dropzone from "svelte-file-dropzone/Dropzone.svelte";
+    import { Textarea } from "$lib/components/ui/textarea";
+    import { Button } from "$lib/components/ui/button";
+    import { text } from "@sveltejs/kit";
     const orgid = "82e2f8eb-b9ef-469c-8678-27211299b6ba";
     let files = {
         accepted: [],
@@ -30,6 +33,15 @@
             reader.readAsText(file);
         }
     }
+    let query = "";
+    let qans = "";
+    async function ask() {
+        const res = await axios.post(`/api/search`, {
+            text: query,
+        });
+        qans = res.data;
+        query = query;
+    }
 </script>
 
 <label for="avatar">Upload a picture:</label>
@@ -42,6 +54,17 @@
 />
 <label for="many">Upload multiple files of any type:</label>
 <input bind:files id="many" multiple type="file" />
-<button on:click={Upload}>data</button>
-
+<Button on:click={Upload}>upload file</Button>
 <Dropzone on:drop={handleFilesSelect} />
+
+<Textarea bind:value={query}></Textarea>
+<Button on:click={ask}>ask</Button>
+
+<div class="flex flex-col">
+    {#each qans as an}
+        <p>
+            {an.secdata}
+        </p>
+        <br />
+    {/each}
+</div>
