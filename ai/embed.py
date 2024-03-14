6fe579819231319b786af32359c9ca2b03f2bdf9
abbmarand <markus.andersson@abbgymnasiet.se>
2024-03-14 22:22:47 +0100

    faster API with retry logic

--- ai/embed.py
+++ ai/embed.py
@@ -1,10 +1,12 @@
 from transformers import AutoTokenizer, AutoModel
 import torch
 import torch.nn.functional as F
+
 class SentenceTransformer:
-    def __init__(self):
+    def __init__(self, device='cuda' if torch.cuda.is_available() else 'cpu'):
+        self.device = device
         self.tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
-        self.model = AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
+        self.model = AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2').to(self.device)
 
     def mean_pooling(self, model_output, attention_mask):
         token_embeddings = model_output[0] # First element of model_output contains all token embeddings
@@ -12,7 +14,7 @@ class SentenceTransformer:
         return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
     def calculatevec(self, sentence):
-        encoded_input = self.tokenizer(sentence, padding=True, truncation=True, return_tensors='pt')
+        encoded_input = self.tokenizer(sentence, padding=True, truncation=True, return_tensors='pt').to(self.device)
         with torch.no_grad():
             model_output = self.model(**encoded_input)
 
