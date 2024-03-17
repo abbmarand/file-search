20d179bae5e11c7aff558e920dd25cdc9b2e0e0c
abbmarand <markus.andersson@abbgymnasiet.se>
2024-03-17 19:54:25 +0100

    sortby table

--- web/src/routes/upload/data-table.svelte
+++ web/src/routes/upload/data-table.svelte
@@ -7,9 +7,12 @@
     } from "svelte-headless-table";
     import { writable } from "svelte/store"; // Import writable store
     import * as Table from "$lib/components/ui/table";
+    import { addPagination, addSortBy } from "svelte-headless-table/plugins";
+    import ArrowUpDown from "lucide-svelte/icons/arrow-up-down";
     import DataTableActions from "./data-table-actions.svelte";
     import axios, { type AxiosResponse } from "axios";
     import { onMount } from "svelte";
+    import { Button } from "$lib/components/ui/button";
 
     type File = {
         filename: string;
@@ -44,13 +47,12 @@
                 dat.time = `${dat.time}s`;
                 data.update((items) => [...items, dat]); // Update data array
             }
-            table = createTable(data); // Pass data store to createTable
+            init = true;
+            table = createTable(data, {
+                sort: addSortBy(),
+            });
 
             columns = table.createColumns([
-                table.column({
-                    accessor: "fileid",
-                    header: "ID",
-                }),
                 table.column({
                     accessor: "filename",
                     header: "Name",
@@ -58,18 +60,38 @@
                 table.column({
                     accessor: "time",
                     header: "Time",
+                    plugins: {
+                        sort: {
+                            disable: false,
+                        },
+                    },
                 }),
                 table.column({
                     accessor: "date",
                     header: "Date",
+                    plugins: {
+                        sort: {
+                            disable: false,
+                        },
+                    },
                 }),
                 table.column({
                     accessor: "totalsubfiles",
                     header: "Subfiles",
+                    plugins: {
+                        sort: {
+                            disable: false,
+                        },
+                    },
                 }),
                 table.column({
                     accessor: "state",
                     header: "State",
+                    plugins: {
+                        sort: {
+                            disable: false,
+                        },
+                    },
                 }),
                 table.column({
                     accessor: ({ fileid }) => fileid,
@@ -85,7 +107,6 @@
 
             ({ headerRows, pageRows, tableAttrs, tableBodyAttrs } =
                 table.createViewModel(columns));
-            init = true;
         } catch (error) {
             console.error("Error fetching data:", error);
         }
@@ -104,9 +125,52 @@
                                     attrs={cell.attrs()}
                                     let:attrs
                                     props={cell.props()}
+                                    let:props
                                 >
                                     <Table.Head {...attrs}>
-                                        <Render of={cell.render()} />
+                                        {#if cell.id === "date"}
+                                            <Button
+                                                variant="ghost"
+                                                on:click={props.sort.toggle}
+                                            >
+                                                <Render of={cell.render()} />
+                                                <ArrowUpDown
+                                                    class={"ml-2 h-4 w-4"}
+                                                />
+                                            </Button>
+                                        {:else if cell.id === "time"}
+                                            <Button
+                                                variant="ghost"
+                                                on:click={props.sort.toggle}
+                                            >
+                                                <Render of={cell.render()} />
+                                                <ArrowUpDown
+                                                    class={"ml-2 h-4 w-4"}
+                                                />
+                                            </Button>
+                                        {:else if cell.id === "totalsubfiles"}
+                                            <Button
+                                                variant="ghost"
+                                                on:click={props.sort.toggle}
+                                            >
+                                                <Render of={cell.render()} />
+                                                <ArrowUpDown
+                                                    class={"ml-2 h-4 w-4"}
+                                                />
+                                            </Button>
+                                        {:else if cell.id === "state"}
+                                            <Button
+                                                variant="ghost"
+                                                on:click={props.sort.toggle}
+                                            >
+                                                <Render of={cell.render()} />
+                                                <ArrowUpDown
+                                                    class={"ml-2 h-4 w-4"}
+                                                />
+                                            </Button>
+                                        {:else}
+                                            <Render of={cell.render()} />
+                                        {/if}
                                     </Table.Head>
                                 </Subscribe>
                             {/each}
